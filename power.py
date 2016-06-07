8d9f723821a3d5386391e7759936020aca0206f7
Pradeep Gokul <pradeepgm96@outlook.com>
2016-06-07 14:27:42 +0530

    Update power.py

--- power.py
+++ power.py
@@ -1,5 +1,12 @@
 n=input()
-if(int(n)%2==0):
+m=int(n/2)
+count=0
+for i in range(3,m+1):
+    if(n%i==0):
+        count=1
+    if(i%2==0 and count==1):
+        count=0
+if((int(n)%2==0)and(count==0)):
     print("true")
 else:
-    print("false")
+    print("false"))
